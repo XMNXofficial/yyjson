2bb8999a5f2498878aaa01fc41c213d88e2f71a8
XMNX <mchaohao159@outlook.com>
2023-07-03 04:03:20 +0800

    1

--- StateEdit.cpp
+++ StateEdit.cpp
@@ -8,18 +8,24 @@ int main()
     yyjson_mut_val *root = yyjson_mut_obj(doc);
     yyjson_mut_doc_set_root(doc, root);
 
-    // StateEditArr
-    yyjson_mut_val *key_arr_StateEdit = yyjson_mut_str(doc, "StateEdit");
-    yyjson_mut_val *val_arr_StateEdit = yyjson_mut_arr(doc);
-    yyjson_mut_obj_add(root, key_arr_StateEdit, val_arr_StateEdit);
-    // StateEditArr-MemberObj
+    // StatEditArr
+    yyjson_mut_val *key_arr_StatEdit = yyjson_mut_str(doc, "StatEdit");
+    yyjson_mut_val *val_arr_StatEdit = yyjson_mut_arr(doc);
+    yyjson_mut_obj_add(root, key_arr_StatEdit, val_arr_StatEdit);
+    // StatEditArr-MemberObj
 
-    yyjson_mut_val *val_arr_StateEdit_obj_Member =  yyjson_mut_arr_add_obj(doc,val_arr_StateEdit);
+    yyjson_mut_val *val_arr_StatEdit_obj_Member =  yyjson_mut_arr_add_obj(doc,val_arr_StatEdit);
    
 
-    yyjson_mut_val *key_arr_StateEdit_obj_Member_str_key_ScriptName = yyjson_mut_str(doc, "Script");
-    yyjson_mut_val *key_arr_StateEdit_obj_Member_str_val_ScriptName = yyjson_mut_str(doc, "佩里科岛");
-    yyjson_mut_obj_add(val_arr_StateEdit_obj_Member,key_arr_StateEdit_obj_Member_str_key_ScriptName,key_arr_StateEdit_obj_Member_str_val_ScriptName);
+    yyjson_mut_val *key_arr_StatEdit_obj_Member_str_key_ScriptName = yyjson_mut_str(doc, "Script");
+    yyjson_mut_val *key_arr_StatEdit_obj_Member_str_val_ScriptName = yyjson_mut_str(doc, "佩里科岛");
+    yyjson_mut_obj_add(val_arr_StatEdit_obj_Member,key_arr_StatEdit_obj_Member_str_key_ScriptName,key_arr_StatEdit_obj_Member_str_val_ScriptName);
+
+    yyjson_mut_val *key_arr_StatEdit_obj_Member_str_key_Stat = yyjson_mut_str(doc, "Stat");
+    yyjson_mut_val *key_arr_StatEdit_obj_Member_str_val_Stat = yyjson_mut_arr(doc);
+    yyjson_mut_obj_add(val_arr_StatEdit_obj_Member,key_arr_StatEdit_obj_Member_str_key_Stat,key_arr_StatEdit_obj_Member_str_val_Stat);
+
+    
 
     cout << yyjson_mut_write(doc, YYJSON_WRITE_PRETTY, 0);
     return 0;
